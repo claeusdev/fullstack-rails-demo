63711adbdfdd7a74944b09f347d98869ec7553e5
Nana Adjei Manu <claeusdev@gmail.com>
2018-07-14 09:48:05 +0000

    Add rails default frontend magic

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -2,3 +2,8 @@ import "init";
 import "components/page/page";
 import "components/auth-form/auth-form";
 import "components/chat/chat";
+import Turbolinks from "turbolinks";
+import Rails from "rails-ujs";
+
+Rails.start();
+Turbolinks.start();
